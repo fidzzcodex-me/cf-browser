29042f5b8780a3bbb265ac8a0fbed9b4436343d2
fidzzcodex.me <rinatonidi28@gmail.com>
2026-09-02 15:29:19 +0700

    Upload: replace src/index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 const { connect, _pageController }                                   = require("./browser");
 const { bypass }                                                     = require("./bypass");
+const { engine, handlerMap }                                         = require("./engine");
 const { applyPatches }                                               = require("./patches/runtime-fix");
 const { injectStealthScripts }                                       = require("./stealth/fingerprint");
 const { applyAll: applyEvasions }                                    = require("./stealth/evasions");
@@ -12,10 +13,18 @@ const { checkTurnstile, turnstileSolverLoop }                        = require("
 const { solveRecaptchaV2Audio }                                      = require("./captcha/recaptcha");
 const { solveAudioCaptcha }                                          = require("./captcha/stt");
 const { createVirtualDisplay, destroyVirtualDisplay }                = require("./display");
+const { log, sleep, formatCookie, safeGoto, waitCfClearance, waitToken, getCfClearance } = require("./wait");
+const { detectChallengeType, getChallengeType, isChallengePage, waitChallengeCleared }   = require("./detect");
+const { extractSiteKey, isUUID }                                     = require("./extract");
+const { saveSession, listSessions, SESSION_DIR }                     = require("./session");
+const { FAKE_TURNSTILE, FAKE_HCAPTCHA }                              = require("./pages");
+const { connectBrowser }                                             = require("./engine-browser");
 
 module.exports = {
   connect,
   bypass,
+  engine,
+  handlerMap,
   _pageController,
   applyPatches,
   injectStealthScripts,
@@ -32,4 +41,23 @@ module.exports = {
   solveAudioCaptcha,
   createVirtualDisplay,
   destroyVirtualDisplay,
+  log,
+  sleep,
+  formatCookie,
+  safeGoto,
+  waitCfClearance,
+  waitToken,
+  getCfClearance,
+  detectChallengeType,
+  getChallengeType,
+  isChallengePage,
+  waitChallengeCleared,
+  extractSiteKey,
+  isUUID,
+  saveSession,
+  listSessions,
+  SESSION_DIR,
+  FAKE_TURNSTILE,
+  FAKE_HCAPTCHA,
+  connectBrowser,
 };
